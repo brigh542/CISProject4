bb7d83ea4bee78c5a9fc07c87a5095e228965904
Zeus-The-Cat <8905735+Zeus-The-Cat@users.noreply.github.com>
2017-11-27 17:22:56 -0500

    Added tryPaths
    First part of project4 is done now the hard part, redirecting the input/output and executing the commands

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
+#include <dirent.h>
 
 #define TOKEN_DELIM " \n\r\t\a"
 
@@ -10,15 +12,19 @@
 //char * strcat(char * dest, char * src) -> append src to dest
 //int strcmp(char * s1, char * s2) -> 0 means equal
 //char * strtok(char *restrict str, const char *restrict sep) -> splits string
+//int execve(const char *filename, char *const argv[], char *const envp[])
+
 
 char ** parseString(char line[]);
+int tryPaths(char ** paths, char * command);
 
 int main(){
-  //char line[] = "Dakotah\rMade This to\ntest spaces";
   char ** stringArray;
-  char * line = NULL;
+  char * line = NULL, p;
   size_t linecap = 0;
   ssize_t linelen;
+  char * paths[3] = {"/bin","/sbin",NULL};
+  int tempInt;
   //Loop for input
   while(1){
     printf("$:");
@@ -30,11 +36,24 @@ int main(){
         printf("Quitting...\n");
         break;
       }else if(0){
-        //path internal command
+        //--You can ignore this I made it for reference--
+        // int pid;
+        // if((pid = fork()) == 0){
+        //   printf("Child: ");
+        //   printf("%s\n",stringArray[0]);
+        //   break;
+        // }else{
+        //   int one = 1;
+        //   wait(&one);
+        //   printf("Parent: ");
+        //   printf("%s\n",stringArray[0]);
+        // }
+        //------------------------------------------------
       }else if(0){
         //cd internal command
-      }else if(0){
+      }else if((tempInt = tryPaths(paths,stringArray[0])) >= 0){
         //system commands
+        printf("Found command in %s.\n",paths[tempInt]);
       }else{
         printf("%s :Command not recognized.\n",stringArray[0]);
       }
@@ -63,4 +82,32 @@ char ** parseString(char line[]){
     }
   }
   return charArray;
+}
+
+//Given array of path names, and command name
+//-> returns -1 if no path contains command a
+//-> returns n >= 0, the position in paths, if command found
+int tryPaths(char ** paths, char * command){
+  int location = -1;
+  DIR *dir_ptr;
+  struct dirent *direntp;
+  int position = 0, found = 1;
+  while(paths[position] != NULL && found){
+    if((dir_ptr = opendir(paths[position])) != NULL){
+      //opened directory indicated by paths[location]
+      while((direntp = readdir(dir_ptr)) != NULL && found){
+        if(strcmp(direntp->d_name,command) == 0){
+          //command found
+          found = 0;
+          location = position;
+        }
+      }
+      closedir(dir_ptr);
+    }else{
+      //couldn't open the directory indicated by paths
+      printf("Directory at %s couldn't be accessed.\n",paths[position]);
+    }
+    position++;
+  }
+  return location;
 }
