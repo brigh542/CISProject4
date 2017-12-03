a48c6bd0d2cfcb67e1fa227833bc2c229715bc44
Zeus-The-Cat <8905735+Zeus-The-Cat@users.noreply.github.com>
2017-12-03 14:16:51 -0500

    updated main.c
    Removed prototypes, method implementations, and added imports

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
-
-#define TOKEN_DELIM " \n\r\t\a"
+#include <fcntl.h>
+#include <help.h>
 
 //These are for reference purposes
 //size_t strlen(char * s) -> returns size of s without NULL
@@ -13,10 +13,6 @@
 //int strcmp(char * s1, char * s2) -> 0 means equal
 //char * strtok(char *restrict str, const char *restrict sep) -> splits string
 //int execve(const char *filename, char *const argv[], char *const envp[])
-
-
-char ** parseString(char line[]);
-int tryPaths(char ** paths, char * command);
 
 int main(){
   char ** stringArray;
@@ -59,9 +55,7 @@ int main(){
         else if (strcmp(stringArray[0], "cd")) {
           changeDir((const char*)stringArray[1]);
           continue;
-          
      }else if(strcmp("path", stringArray[0])==0){
-
 		      if(stringArray[1] != NULL){
 			      if(strcmp("+", stringArray[1])==0){
 				      path_add(path, );
@@ -73,10 +67,23 @@ int main(){
 		      else
 			        path();
 
-
       }else if((tempInt = tryPaths(paths,stringArray[0])) >= 0){
         //system commands
-        printf("Found command in %s.\n",paths[tempInt]);
+        //printf("Found command in %s.\n",paths[tempInt]);
+	int outputPos,pipePos;
+      	if((outputPos = findString(stringArray,">")) >= 0){
+      		//Has > so call outputRedirect
+      		//printf("Found > at %d.\n",outputPos);
+      		outputRedirect(stringArray,paths[tempInt],outputPos);
+      	}else if((pipePos = findString(stringArray,"|")) >= 0){
+      		//Doesn't have > check for |
+      		printf("Found | at %d.\n",pipePos);
+
+      	}else{
+      		//Doesn't have either | nor > so normal execute
+      		printf("Neither > nor | Found.\n");
+
+      	}
       }else{
         printf("%s :Command not recognized.\n",stringArray[0]);
       }
@@ -86,128 +93,4 @@ int main(){
   }
   free(stringArray);
   return 0;
-}
-
-//**Need to make this dynamically allocate based on input size
-//Parses a given string based on TOKEN_DELIM
-char ** parseString(char line[]){
-  int maxEntries = 100, iterator = 0;
-  char * stringArray[maxEntries];
-  char * token;
-  token = strtok(line,TOKEN_DELIM);
-  while(token != NULL){
-    //printf("token: %s\n",token); //debug:To show what is being read
-    stringArray[iterator] = token;
-    iterator++;
-    if(iterator <= maxEntries){
-      token = strtok(NULL,TOKEN_DELIM);
-    }else{
-      token = NULL;
-    }
-  }
-  stringArray[iterator] = NULL;
-  //printf("it: %d string: %s\n",iterator,stringArray[iterator]);
-  //Need to check for strings --> cm1 "output string with"
-  //Cases: find " within string then add them together until another " is found
-  char ** finalArray = (char**)malloc(sizeof(char *)* maxEntries);
-  int it1 = 0,it2 = 0,it3 = 0,found = 0,locationFound = -1;
-  while(1){
-    //printf("looped\n");
-    if(stringArray[it1] != NULL){
-      //printf("w1: %s\n",stringArray[it1]);
-      while(1){
-        if(stringArray[it1][it2] == '\0'){
-          if(found){
-            if(locationFound != it1){
-              //append current string to original(locationFound)
-              char * tempString = (char *)malloc(sizeof(char)*((int)strlen(stringArray[locationFound])+(int)strlen(stringArray[it1])+2));
-              strcat(tempString,stringArray[locationFound]);
-              stringArray[locationFound] = tempString;
-              strcat(stringArray[locationFound]," ");
-              strcat(stringArray[locationFound],stringArray[it1]);
-            }else if(finalArray[it3+1] == NULL){
-              finalArray[it3] = stringArray[it1];
-              it3++;
-            }
-          }else if(!found){
-            finalArray[it3] = stringArray[it1];
-            it3++;
-          }
-          it1++;
-          it2 = 0;
-          break;
-        }else{
-          if(stringArray[it1][it2] == '\"' && !found){
-            //found first "
-            found = 1;
-            locationFound = it1;
-          }else if(stringArray[it1][it2] == '\"'){
-            //found second "
-            if(locationFound != it1){
-              //append current string to original(locationFound)
-              char * tempString = (char *)malloc(sizeof(char)*((int)strlen(stringArray[locationFound])+(int)strlen(stringArray[it1])+2));
-              strcat(tempString,stringArray[locationFound]);
-              stringArray[locationFound] = tempString;
-              strcat(stringArray[locationFound]," ");
-              strcat(stringArray[locationFound],stringArray[it1]);
-              finalArray[it3] = stringArray[locationFound];
-              //printf("s%d: %s\n",it3,finalArray[it3]);
-              locationFound = -1;
-              it3++;
-              found = 0;
-              it1++;
-              it2 = 0;
-              break;
-            }else{
-              found = 0;
-              locationFound = -1;
-            }
-          }
-        }
-        it2++;
-      }
-    }else{
-      //printf("Exiting masterLoop\n");
-      break;
-    }
-  }
-  it1= 0;
-  while(1){
-    if(finalArray[it1] == NULL){
-      break;
-    }else{
-      printf("s%d: %s\n",it1,finalArray[it1]);
-    }
-    it1++;
-  }
-  return finalArray;
-}
-
-
-//Given array of path names, and command name
-//-> returns -1 if no path contains command a
-//-> returns n >= 0, the position in paths, if command found
-int tryPaths(char ** paths, char * command){
-  int location = -1;
-  DIR *dir_ptr;
-  struct dirent *direntp;
-  int position = 0, found = 1;
-  while(paths[position] != NULL && found){
-    if((dir_ptr = opendir(paths[position])) != NULL){
-      //opened directory indicated by paths[location]
-      while((direntp = readdir(dir_ptr)) != NULL && found){
-        if(strcmp(direntp->d_name,command) == 0){
-          //command found
-          found = 0;
-          location = position;
-        }
-      }
-      closedir(dir_ptr);
-    }else{
-      //couldn't open the directory indicated by paths
-      printf("Directory at %s couldn't be accessed.\n",paths[position]);
-    }
-    position++;
-  }
-  return location;
 }
