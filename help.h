bd260c6db7272a8c945976eace13f7446167a0d1
brigh542 <31809756+brigh542@users.noreply.github.com>
2017-12-03 17:05:29 -0500

    Update help.h

--- help.h
+++ help.h
@@ -1,6 +1,36 @@
+#ifndef __HELP_H__
+#define __HELP_H__
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define TOKEN_DELIM " \n\r\t\a"
+
+char *pathName;
+
+
+
 //Helper file for help.c
-int changeDir(const char* name);
+
 char ** parseString(char line[]);
 int tryPaths(char ** paths, char * command);
 int outputRedirect(char ** stringArray, char * path, int redirectPos);
 int findString(char ** stringArray, char * string);
+//-------------------------------------------------------------------
+void path();
+void subPath(char* temp);
+void addPath(char* temp);
+void append(char* a, char b);
+
+
+
+
+#endif 
