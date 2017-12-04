169e25771f6b36a897ad8840fba583ab57a31835
Zeus-The-Cat <8905735+Zeus-The-Cat@users.noreply.github.com>
2017-12-03 20:32:35 -0500

    update 2
    forgot chdir

--- help.c
+++ help.c
@@ -1,6 +1,19 @@
 
 #include "help.h"
 
+int changeDir(const char* name){
+	if(name == NULL){
+		chdir(getenv("HOME"));
+		return 1;
+	}
+	else{
+		if(chdir(name) == -1){
+			printf("There is no directory called: %s\n", name);
+			return -1;
+		}
+	}
+	return 0;
+}
 
 void path(){
 	
